812169756d6dcc851a0b055b208a4281d3cdd801
Evan <evan@Evans-Mac.local>
2016-04-21 15:29:54 -0700

    finished the calculator

--- CalcApp/CalcApp/ViewController.swift
+++ CalcApp/CalcApp/ViewController.swift
@@ -120,17 +120,23 @@ class ViewController: UIViewController {
     //compouds number and appends to array if needed
     //TODO: need to include shit that does the toggle
     func compound(incoming: String) {
+        self.resultLabel.text = ""
         self.data.curr = self.data.curr + incoming
+        self.currentLabel.text = self.currentLabel.text! + incoming
     }
     
     func harvest(incoming: String) {
-        let num = Double(self.data.curr)
-        self.data.arr.append(num!)
+        if self.data.curr != "" {
+            let num : Double = Double(self.data.curr)!
+            self.data.arr.append(num)
+        }
         self.data.curr = ""
-        if incoming == "=" {
-            calculate()
-        } else {
+        
+        if incoming != "=" {
             self.data.op = incoming
+            self.currentLabel.text = self.currentLabel.text! + incoming
+        } else {
+            calculate()
         }
         
     }
@@ -140,8 +146,48 @@ class ViewController: UIViewController {
         //do shit
         //actually calculates
         
+        let oper = self.data.op
+        var total : Double = 0
+        
+        if oper == "add" { // add
+            total = self.data.arr[0] + self.data.arr[1]
+            
+        } else if oper == "sub" { // sub
+            total = self.data.arr[0] - self.data.arr[1]
+            
+        } else if oper == "mul" { //mul
+            total = self.data.arr[0] * self.data.arr[1]
+            
+        } else if oper == "div" {//div
+            total = self.data.arr[0] / self.data.arr[1]
+            
+        } else if oper == "mod" { //mod
+            total = self.data.arr[0] % self.data.arr[1]
+            
+        } else if oper == "avg" { //avg
+            for num in self.data.arr {
+                total = total + num
+            }
+            total = total / Double(self.data.arr.count)
+            
+        } else if oper == "count" { //count
+            total = Double(self.data.arr.count)
+            
+        } else if oper == "fact" { // fact
+            var start = Int(self.data.arr[0])
+            total = Double(start)
+            start = start - 1
+            while start > 0 {
+                total = total * Double(start)
+                start = start - 1
+            }
+        }
+        
+        self.resultLabel.text = String(total)
+        
         //end of method
         self.data.clear()
+        self.currentLabel.text = ""
         self.data.op = ""
     }
     
